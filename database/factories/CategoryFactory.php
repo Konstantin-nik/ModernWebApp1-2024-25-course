e7d695681a46040540633dca1cd544ab7820f64d
Konstantin-nik <78786301+Konstantin-nik@users.noreply.github.com>
2024-11-14 19:31:29 +0100

    👌 IMPROVE: add user factory

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -17,8 +17,8 @@ class CategoryFactory extends Factory
     public function definition(): array
     {
         return [
-            "name" => $this->faker->name,
-            "description" => $this->faker->sentence,
+            "name" => fake()->unique()->colorName(),
+            "description" => fake()->sentence(),
         ];
     }
 }
