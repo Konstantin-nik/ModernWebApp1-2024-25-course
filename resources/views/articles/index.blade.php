e66a3e1e612144a0cb63b748a3c73fb9c7826a8b
Konstantin-nik <78786301+Konstantin-nik@users.noreply.github.com>
2024-11-15 11:35:51 +0100

    👌 IMPROVE: article layout + link to article index

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -1,5 +1,5 @@
 <x-main-layout >
     @foreach ($articles as $article)
-        <h2 mt-2>{{ $article->title }}</h2>
+        <h2 class="mt-2 font-semibold"><a href="{{ route('articles.show', $article->id) }}">{{ $article->title }}</a></h2>
     @endforeach
 </x-main-layout>
--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -1,4 +1,4 @@
 <x-main-layout >
-    <h2>{{ $article->title }}</h2>
+    <h2 class="font-bold">{{ $article->title }}</h2>
     <div>{{ $article->content }}</div>
 </x-main-layout>
